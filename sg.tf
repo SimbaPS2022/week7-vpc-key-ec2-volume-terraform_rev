e803f9d9ccba1fb1c8c22ddd35d2b2b2fcc22e41
Prosper <Simbapaoude@gmail.com>
2026-03-29 05:00:27 -0400

    terraform code for utc app tk 5678

---
+++ sg.tf
@@ -0,0 +1,42 @@
+// security group 
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-security-group"
+  description = "Security group for web servers with ports 22, 80, and 8080 open"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
