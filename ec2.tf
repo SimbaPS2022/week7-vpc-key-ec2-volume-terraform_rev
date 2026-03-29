41195e9f61ffea718127bf73fa686d0099bc46bf
Prosper <Simbapaoude@gmail.com>
2026-03-29 05:30:54 -0400

    add s3 bucket code tk 4589

--- ec2.tf
+++ ec2.tf
@@ -18,9 +18,9 @@ resource "aws_instance" "server" {
 // ebs volume 
 
 resource "aws_ebs_volume" "ebs" {
-  size = 20 
+  size              = 20
   availability_zone = aws_instance.server.availability_zone
-  
+
   tags = {
     Name = "Extra-volume"
   }
@@ -28,7 +28,7 @@ resource "aws_ebs_volume" "ebs" {
 
 resource "aws_volume_attachment" "att" {
   instance_id = aws_instance.server.id
-  volume_id = aws_ebs_volume.ebs.id
+  volume_id   = aws_ebs_volume.ebs.id
   device_name = "/dev/sdb"
-  
+
 }
